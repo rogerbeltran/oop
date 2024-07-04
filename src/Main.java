12dc9b8fa10385d904f6bf387921f210dddc0901
rogerbeltran <rbeltranv@est.ups.edu.ec>
2024-07-03 21:46:58 -0500

    Add(IV): printSquareTriangle Method

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@
 public class Main {
     public static void main(String[] args) {
         //printLine( 10);
-        printSquare(6);
-
+        //printSquare(6);
+        printTriangle(3);
+        printTriangle(5);
+        printTriangle(7);
     }
     //  public static void printLine(int n){
     //    for(int i = 0; i < n; i++) {
@@ -15,12 +17,22 @@ public class Main {
     //System.out.print("* ");
 
     //}
-    public static void printSquare(int a) {
-        for (int b = 0; b < a; b++) {
-            for (int c = 0; c < a; c++)
-                System.out.print("* ");
+    //public static void printSquare(int a) {
+    //  for (int b = 0; b < a; b++) {
+    //  for (int c = 0; c < a; c++)
+    //   System.out.print("* ");
+    //      System.out.println();
+    //}
+    //}
+    public static void printTriangle(int T) {
+        System.out.println( "---------------Printing Triangle-----------------------");
+        for (int Altura = 0; Altura < T; Altura++) {
+            for (int Ancho = 0; Ancho < T; Ancho++) {
+                if (Ancho <= Altura) {
+                    System.out.print(" * ");
+                }
+            }
             System.out.println();
-            //}
         }
     }
 }
