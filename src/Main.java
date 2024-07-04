b4c1179f3ce27695db98af9eb3e7226ca1184f90
rogerbeltran <rbeltranv@est.ups.edu.ec>
2024-07-03 21:39:08 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        printLine( 10);
+    }
+    public static void printLine(int n){
+        for(int i = 0; i < n; i++) {
+            System.out.print("* ");
+
+        }
+    }
+}
